79c614a9c292c98ca12dfe54e41415062392c731
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-07-05 17:00:33 +0500

    fizzBuzz

---
+++ main.js
@@ -0,0 +1,11 @@
+var son = prompt("Type a number");
+
+if (son % 3 == 0 && son % 5 == 0) {
+    alert("FizzBuzz")
+}
+else if (son % 3 == 0) {
+    alert("Fizz")
+}
+else if (son % 5 == 00) {
+    alert("Buzz")
+};
